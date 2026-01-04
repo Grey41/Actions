762817a24448a62232bb2f7505f9f3bf2f806761
Grey41 <reuben_ford@hotmail.com>
2026-01-04 20:00:18 +0000

    test

--- wheel.py
+++ wheel.py
@@ -1,4 +1,4 @@
-import subprocess, sysconfig, pathlib, sys, zipfile, shutil, hashlib, base64, packaging.tags
+import subprocess, sysconfig, pathlib, sys, zipfile, shutil, hashlib, base64, packaging.tags, platform
 
 VERSION = "3.1"
 
@@ -78,9 +78,11 @@ def build_wheel(wheel_directory, config_settings = None, metadata_directory = No
     tag = str(next(packaging.tags.sys_tags()))
 
     wheel = f"JoBase-{VERSION}-{tag}.whl"
+    build = "build/" + platform.machine()
     file = zipfile.ZipFile(pathlib.Path(wheel_directory) / wheel, "w")
-    cmake = [] if sys.platform != "win32" else ["-A", "x64"] if sys.maxsize > 2 ** 32 else ["-A", "Win32"]
+
     lines = []
+    cmake = [] if sys.platform != "win32" else ["-A", "x64"] if sys.maxsize > 2 ** 32 else ["-A", "Win32"]
 
     # cmake = ([] if sys.maxsize > 2 ** 32 or sys.platform != "win32" else ["-A", "Win32"]) + [
     #     f"-DCMAKE_PREFIX_PATH={sdl};{mix}",
@@ -112,14 +114,17 @@ def build_wheel(wheel_directory, config_settings = None, metadata_directory = No
     #     subprocess.run(["cmake", "-S", "lib/mix", "-B", "lib/mix/build", *cmake, "-DSDLMIXER_FLAC_LIBFLAC=OFF"])
     #     subprocess.run(["cmake", "--build", "lib/mix/build", "--config", "Release"])
 
-    subprocess.run(["cmake", "-S", ".", "-B", tag, *cmake,
+    print("BASE", base)
+    print("CMAKE", cmake)
+    #"-S", ".", "-B", tag, 
+
+    subprocess.run(["cmake", "-S", ".", "-B", build, *cmake,
         "-DPython3_ROOT_DIR=" + base,
         "-DJOBASE_EXT=" + ext,
         "-DJOBASE_DIR=" + str(pathlib.Path(wheel_directory))
     ])
 
-    subprocess.run(["cmake", "--build", tag, "--config", "Release"])
-    print("---------", str(pathlib.Path(wheel_directory)), ext)
+    subprocess.run(["cmake", "--build", build, "--config", "Release", "--verbose"])
     subprocess.run(["ls", str(pathlib.Path(wheel_directory))])
 
     for path in pathlib.Path("module").rglob("*"):
